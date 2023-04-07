43a6efe1ab198b94d6c708bcdc7df53be30d10bd
Heizal <patriciaheizal@gmail.com>
2023-04-07 17:01:49 +0200

    add update and delete method

---
+++ app/tests/note/test_models.py
@@ -0,0 +1,25 @@
+from app.extensions.database import db
+from app.notes.models import Note
+
+def test_note_update(client):
+    # updates note properties
+    note = Note(slug='i-want', title='Sugar', content='Lorem ipsum dolor sit amet consectetur adipisicing.')
+    db.session.add(note)
+    db.session.commit()
+
+    note.title = 'Eggs'
+    note.save()
+
+    updated_note = Note.query.filter_by(slug='i-want').first()
+    assert updated_note.title == 'Eggs'
+
+def test_note_delete(client):
+    #deletes note
+    note = Note(slug='butter', title='Butter', content='Lorem ipsum dolor sit amet consectetur adipisicing')
+    db.session.add(note)
+    db.session.commit()
+
+    note.delete()
+
+    deleted_note = Note.query.filter_by(slug='butter').first()
+    assert deleted_note is None
